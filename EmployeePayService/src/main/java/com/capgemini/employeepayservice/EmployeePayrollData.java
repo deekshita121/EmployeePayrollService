06ae5fa5f66599c08e058b2b79e276fa5239ee2c
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-19 12:37:22 +0530

    [deekshita] Add : Create an employee payroll service to read and write employee payroll to a console

--- EmployeePayService/src/main/java/com/capgemini/employeepayservice/EmployeePayrollData.java
+++ EmployeePayService/src/main/java/com/capgemini/employeepayservice/EmployeePayrollData.java
@@ -2,4 +2,17 @@ package com.capgemini.employeepayservice;
 
 public class EmployeePayrollData {
 
+	public int id;
+	public String name;
+	public double salary;
+	
+	public EmployeePayrollData(Integer id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	} 
+	
+	public String toString() {
+		return "id = " + id + ", name = " + '\'' + name + '\'' + ", salary = " + salary;
+	}
 }
--- EmployeePayService/src/main/java/com/capgemini/employeepayservice/EmployeePayrollService.java
+++ EmployeePayService/src/main/java/com/capgemini/employeepayservice/EmployeePayrollService.java
@@ -1,13 +1,42 @@
 package com.capgemini.employeepayservice;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class EmployeePayrollService 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	private static final Logger log = LogManager.getLogger(EmployeePayrollService.class);
+
+	private List<EmployeePayrollData> employeePayrollList;
+	public EmployeePayrollService() {}
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
+	}
+
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		log.info("Enter Employee ID : ");
+		int id = consoleInputReader.nextInt();
+		log.info("Enter Employee Name : ");
+		String name = consoleInputReader.next();
+		log.info("Enter Employee Salary : ");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
+	
+	private void writeEmployeePayrollData() {
+		log.info("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+	}
 }
