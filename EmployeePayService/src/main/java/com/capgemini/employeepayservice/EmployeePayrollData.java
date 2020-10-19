cb367bf3c9685013bef5656b89befb9595082ee2
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-19 12:26:51 +0530

    [deekshita] Add : Welcome

---
+++ EmployeePayService/src/main/java/com/capgemini/employeepayservice/EmployeePayrollData.java
@@ -0,0 +1,5 @@
+package com.capgemini.employeepayservice;
+
+public class EmployeePayrollData {
+
+}
---
+++ EmployeePayService/src/main/java/com/capgemini/employeepayservice/EmployeePayrollService.java
@@ -0,0 +1,13 @@
+package com.capgemini.employeepayservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class EmployeePayrollService 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
